d77ddbf8f9365e11219f0f34368108d9b5e0b26c
Didymus <didymus.benson@gmail.com>
2017-10-05 11:03:13 -0600

    yesterday's progress

--- script2.js
+++ script2.js
@@ -6,27 +6,12 @@ if (window.location.search.indexOf('?css=fun') === 0) {
     document.write('<link rel="stylesheet" href="./default2.css" />');
 }
 var file = [];
+var reordered_data = [];
 var spaces;
 var file_name = "default.csv";
 var template = Handlebars.compile(document.querySelector('#template').innerHTML);
 document.querySelector('input').addEventListener('change', getFile);
 var templatequestion = Handlebars.compile(document.querySelector('#templatequestion').innerHTML);
-
-
-
-
-//function getBlank() {
-//    return {
-//        skill: "",
-//        level: "",
-//        questionname: "",
-//        function: "",
-//        topic: "",
-//        difficultylevel: "",
-//        passagetext: "",
-//        questions: [getblankQuestion()],
-//    }
-//}
 
 function getBlankQuestion() {
     return {
@@ -71,9 +56,9 @@ function getBlankQuestion() {
 
 function getBlank() {
     return {
+        passagenum: "",
         skill: "",
         level: "",
-        passagenum: "",
         function: "",
         topic: "",
         difficultylevel: "",
@@ -115,7 +100,6 @@ function addTinyMCE() {
                 }));
                 saveData(textarea);
                 //console.log(textarea.dataset.editortext);
-
             });
         }
     });
@@ -123,11 +107,47 @@ function addTinyMCE() {
 
 function makeUI(data) {
     file = data;
-    var reordered_data = [];
     var unique_passages = [new Set(file.map(item => item.passagenum))];
     console.log(unique_passages);
     for (var i = 0; i < file.length; i++) {
-
+        var questionformat = {
+            questionnum: file[i].questionnum,
+            questionname: file[i].questionname,
+            questionfunction: file[i].questionfunction,
+            questiontext: file[i].questiontext,
+            questiontype: file[i].questiontype,
+            answertext1: file[i].answertext1,
+            answertext2: file[i].answertext2,
+            answertext3: file[i].answertext3,
+            answertext4: file[i].answertext4,
+            answertext5: file[i].answertext5,
+            answertext6: file[i].answertext6,
+            questiontexteditorcomments: file[i].questiontexteditorcomments,
+            questionaudio: file[i].questionaudio,
+            questionimagedescription: file[i].questionimagedescription,
+            questionrubric: file[i].questionrubric,
+            answer1feedback: file[i].answer1feedback,
+            answer1audio: file[i].answer1audio,
+            answer1imagedescription: file[i].answer1imagedescription,
+            answer2feedback: file[i].answer2feedback,
+            answer2audio: file[i].answer2audio,
+            answer2imagedescription: file[i].answer2imagedescription,
+            answer3feedback: file[i].answer3feedback,
+            answer3audio: file[i].answer3audio,
+            answer3imagedescription: file[i].answer3imagedescription,
+            answer4feedback: file[i].answer4feedback,
+            answer4audio: file[i].answer4audio,
+            answer4imagedescription: file[i].answer4imagedescription,
+            answer5feedback: file[i].answer5feedback,
+            answer5audio: file[i].answer5audio,
+            answer5imagedescription: file[i].answer5imagedescription,
+            answer6feedback: file[i].answer6feedback,
+            answer6audio: file[i].answer6audio,
+            answer6imagedescription: file[i].answer6imagedescription,
+            answereditorcomments: file[i].answereditorcomments,
+            Clausespersentence: file[i].Clausespersentence,
+            wordcount: file[i].wordcount
+        };
         if (!reordered_data.some(function (item) {
                 return item.passagenum === file[i].passagenum
             })) {
@@ -145,96 +165,12 @@ function makeUI(data) {
                 references: file[i].references,
                 ERCentralLevel: file[i].ERCentralLevel,
                 ECCentralScore: file[i].ECCentralScore,
-                questions: [{
-                    questionnum: file[i].questionnum,
-                    questionname: file[i].questionname,
-                    questionfunction: file[i].questionfunction,
-                    questiontext: file[i].questiontext,
-                    questiontype: file[i].questiontype,
-                    answertext1: file[i].answertext1,
-                    answertext2: file[i].answertext2,
-                    answertext3: file[i].answertext3,
-                    answertext4: file[i].answertext4,
-                    answertext5: file[i].answertext5,
-                    answertext6: file[i].answertext6,
-                    questiontexteditorcomments: file[i].questiontexteditorcomments,
-                    questionaudio: file[i].questionaudio,
-                    questionimagedescription: file[i].questionimagedescription,
-                    questionrubric: file[i].questionrubric,
-                    answer1feedback: file[i].answer1feedback,
-                    answer1audio: file[i].answer1audio,
-                    answer1imagedescription: file[i].answer1imagedescription,
-
-                    answer2feedback: file[i].answer2feedback,
-                    answer2audio: file[i].answer2audio,
-                    answer2imagedescription: file[i].answer2imagedescription,
-
-                    answer3feedback: file[i].answer3feedback,
-                    answer3audio: file[i].answer3audio,
-                    answer3imagedescription: file[i].answer3imagedescription,
-
-                    answer4feedback: file[i].answer4feedback,
-                    answer4audio: file[i].answer4audio,
-                    answer4imagedescription: file[i].answer4imagedescription,
-
-                    answer5feedback: file[i].answer5feedback,
-                    answer5audio: file[i].answer5audio,
-                    answer5imagedescription: file[i].answer5imagedescription,
-
-                    answer6feedback: file[i].answer6feedback,
-                    answer6audio: file[i].answer6audio,
-                    answer6imagedescription: file[i].answer6imagedescription,
-                    answereditorcomments: file[i].answereditorcomments,
-                    Clausespersentence: file[i].Clausespersentence,
-                    wordcount: file[i].wordcount
-                    }]
+                questions: [questionformat]
             })
         } else {
             for (var j = 0; j < reordered_data.length; j++) {
                 if (reordered_data[j].passagenum === file[i].passagenum) {
-                    reordered_data[j].questions.push({
-                        questionnum: file[i].questionnum,
-                        questionname: file[i].questionname,
-                        questionfunction: file[i].questionfunction,
-                        questiontext: file[i].questiontext,
-                        questiontype: file[i].questiontype,
-                        answertext1: file[i].answertext1,
-                        answertext2: file[i].answertext2,
-                        answertext3: file[i].answertext3,
-                        answertext4: file[i].answertext4,
-                        answertext5: file[i].answertext5,
-                        answertext6: file[i].answertext6,
-                        questiontexteditorcomments: file[i].questiontexteditorcomments,
-                        questionaudio: file[i].questionaudio,
-                        questionimagedescription: file[i].questionimagedescription,
-                        questionrubric: file[i].questionrubric,
-                        answer1feedback: file[i].answer1feedback,
-                        answer1audio: file[i].answer1audio,
-                        answer1imagedescription: file[i].answer1imagedescription,
-
-                        answer2feedback: file[i].answer2feedback,
-                        answer2audio: file[i].answer2audio,
-                        answer2imagedescription: file[i].answer2imagedescription,
-
-                        answer3feedback: file[i].answer3feedback,
-                        answer3audio: file[i].answer3audio,
-                        answer3imagedescription: file[i].answer3imagedescription,
-
-                        answer4feedback: file[i].answer4feedback,
-                        answer4audio: file[i].answer4audio,
-                        answer4imagedescription: file[i].answer4imagedescription,
-
-                        answer5feedback: file[i].answer5feedback,
-                        answer5audio: file[i].answer5audio,
-                        answer5imagedescription: file[i].answer5imagedescription,
-
-                        answer6feedback: file[i].answer6feedback,
-                        answer6audio: file[i].answer6audio,
-                        answer6imagedescription: file[i].answer6imagedescription,
-                        answereditorcomments: file[i].answereditorcomments,
-                        Clausespersentence: file[i].Clausespersentence,
-                        wordcount: file[i].wordcount
-                    })
+                    reordered_data[j].questions.push(questionformat)
                 }
             }
         }
@@ -243,11 +179,14 @@ function makeUI(data) {
 
     for (var i = 0; i < reordered_data.length; i++) {
         if (reordered_data[i].passagenum === "") {
-            reordered_data[i].passagenum = "undeclared";
+            reordered_data[i].passagenum = 9999;
+        } else {
+            // NO ZEROES FOR PASSAGES
+            reordered_data[i].passagenum = i + 1;
         }
         for (var j = 0; j < reordered_data[i].questions.length; j++) {
             if (reordered_data[i].questions[j].questionnum === "") {
-                reordered_data[i].questions[j].questionnum = "undeclared";
+                reordered_data[i].questions[j].questionnum = 9000 + j;
             }
         }
     }
@@ -255,7 +194,7 @@ function makeUI(data) {
     console.log(reordered_data);
 
 
-    //   document.querySelector('#UI').innerHTML = template(data);
+    //document.querySelector('#UI').innerHTML = template(data);
     document.querySelector('#UI').innerHTML = template(reordered_data);
 
     addTinyMCE();
@@ -274,12 +213,12 @@ function makeUI(data) {
 
 // Finds out what the last row in the UI is and then adds another row to the end.
 function add_passage() {
-    //            var new_row = document.createElement('div');
+    //var new_row = document.createElement('div');
     var ui = document.querySelector("#UI");
     var divs = document.querySelectorAll('#UI > div');
     var index;
     if (divs.length < 2) {
-        index = 0;
+        index = 1;
     } else {
         index = parseInt(divs[divs.length - 2].id.split("passage")[1]);
         index++;
@@ -291,14 +230,16 @@ function add_passage() {
     //            console.log(new_rows);
     new_rows.id = "passage" + index;
     new_rows.querySelectorAll(".new_row")[0].onclick = function () {
-        add_question(document.querySelector("#passage" + index));
-    }
+            add_question(document.querySelector("#passage" + index));
+        }
+        // Sets the passage number and question 0 of newly added passages.
+    new_rows.querySelector('h2').innerHTML = "PASSAGE #" + index;
+    new_rows.querySelectorAll('h2')[1].innerHTML = "QUESTION #1";
     ui.insertBefore(new_rows, divs[divs.length - 1]);
     fixNewRow(new_rows.id);
 }
 
 function add_question(passage) {
-
     console.log(passage.id);
     var divs = passage.querySelectorAll('.question');
     console.log(divs, divs.length);
@@ -308,6 +249,8 @@ function add_question(passage) {
     var parser = new DOMParser();
     new_row = parser.parseFromString(new_row, "text/html").querySelector("#question0");
     new_row.id = "question" + next_index;
+    // row ids start at zero, but questions start at 1.
+    new_row.querySelector('h2').innerHTML = "QUESTION #" + (next_index + 1);
     $(new_row).insertAfter(divs[cur_index]);
     fixNewRow(new_row.id);
 }
@@ -338,6 +281,7 @@ function addListeners() {
 
 }
 
+
 function removeRow(id) {
     if (confirm("Are you sure you want to remove this row? Click \"OK\" to continue.") == true) {
         var row = parseInt(id.split("row")[1]);
@@ -351,6 +295,7 @@ function removeRow(id) {
 }
 
 
+//New rows need listeners added to their editors and "remove" buttons.
 function fixNewRow(id) {
     var row = document.querySelector("#" + id);
     var inputs = row.getElementsByTagName("input");
@@ -363,7 +308,6 @@ function fixNewRow(id) {
     addTinyMCE();
     //add change event listener to editors
     var editors = row.querySelectorAll("textarea");
-
     for (var i = 0; i < editors.length; i++) {
         editors[i].addEventListener('keyup', function (event) {
             saveData(this);
@@ -377,15 +321,11 @@ function fixNewRow(id) {
     }
 }
 
-
-
-
 function getFile() {
     var file = this.files[0]
     var reader = new FileReader();
     reader.onload = function (e) {
         var fileData = e.target.result;
-
         //need to fix the spaces in the obj props
         makeUI(d3.csvParse(fileData).map(function (item) {
             spaces = Object.keys(item);
@@ -399,23 +339,22 @@ function getFile() {
         file_name = file_name.split("fakepath")[1].replace("\\", "");
     };
     reader.readAsText(file);
-
 }
 
 function saveData(element) {
-    // Get which row of the CSV to change
+    //Get which row of the CSV to change
     var row = parseInt(element.parentElement.parentElement.parentElement.id.split("row")[1]);
-    //    console.log(row);
+    //console.log(row);
     var columnName = element.previousElementSibling.innerHTML.replace(' ', '');
-    // Adds a new row to the file data if it doesn't exist yet.
+    //Adds a new row to the file data if it doesn't exist yet.
     if (!file[row]) {
-        //        console.log("this is a new row, the last row is:", file[row - 1]);
+        //console.log("this is a new row, the last row is:", file[row - 1]);
         file.push(getBlank());
-        //        console.log(file[row]);
+        //console.log(file[row]);
     }
 
-    //            console.log(row, columnName);
-    //            console.log(file[row][columnName]);
+    //console.log(row, columnName);
+    //console.log(file[row][columnName]);
     if (element.classList.contains("editor")) {
         // DO IT TWICE, BECAUSE MCE
         var columnName = element.previousElementSibling.previousElementSibling.innerHTML.replace(' ', '');
@@ -428,8 +367,7 @@ function saveData(element) {
         file[row][columnName] = element.value;
         // console.log(file[row][columnName])
     }
-
-    //            console.log(file[row][columnName]);
+    //console.log(file[row][columnName]);
 }
 
 
@@ -438,8 +376,46 @@ function downloadit() {
     file = file.filter(function (row) {
         return typeof row.toDelete === 'undefined';
     });
-
-    console.log(file);
+    //console.log(file);
     var exported = d3.csvFormat(file);
-    download(exported, file_name, "text/plain");
+
+    var validity = validate();
+    if (!validity.valid) {
+        var issuesmsg = "This quiz has the following issues:\n";
+        for (var i = 0;i < validity.issues.length; i++){
+            issuesmsg += "\n [" + validity.issues[i].location + ": " + validity.issues[i].issue + "]";
+        }
+        issuesmsg += "\n\n Click \"OK\" to download anyway."
+        if (confirm(issuesmsg) == true) {
+            download(exported, file_name, "text/plain");
+        }
+    } else {
+        download(exported, file_name, "text/plain");
+    }
+}
+
+/*************************************************
+Looks for any reason the user should NOT download
+the file, and marks it in the document. All issues
+are pushed to the "validity" object which is
+returned and presented to the user for review
+before choosing to correct the errors or continue
+to download.
+*************************************************/
+function validate(){
+    var validity = {valid: true, issues: []};
+    //simplify adding issues to validity
+    function add_issue(l, i){
+        validity.issues.push({location: l, issue: i})
+    }
+    //Test issue
+    //add_issue("passage1", "numbering");
+
+
+
+
+    if (validity.issues.length > 0){
+        validity.valid = false;
+    }
+    return validity;
 }
