caa9b40ec8d70d50c2376b6cbbefa2306f04c7a6
Didymus <didymus.benson@gmail.com>
2017-11-07 10:13:32 -0700

    big changes

--- 2.0/2.js
+++ 2.0/2.js
@@ -20,7 +20,76 @@ window.Handlebars.registerHelper('select', function( value, options ){
         return $el.html();
 });
 
-
+///**********************************************************
+// *                   getBlank(string)
+// *
+// * DESC: "gets" a blank object of the specified type.
+// * INPUTS: Option string. Used to determine which object
+// *       to return.
+// * RETURNS: a blank template of the specified object.
+// ***********************************************************/
+//function getBlank() {
+//    var tempuuid = uuidv5("EC_POC", uuidv4())
+//    return {
+//        id: tempuuid,
+//        skill: "",
+//        level: "",
+//        topic: "",
+//        difficultylevel: "",
+//        function: "",
+//        passagetext: "",
+//        passagecorevocabularyused: "",
+//        passagetexteditorcomments: "",
+//        passagetextcommonissuestheme: "",
+//        passageaudio: "",
+//        passageimagedescription: "",
+//        questionname: "",
+//        questionfunction: "",
+//        questioncando: "",
+//        questiontext: "",
+//        questiontextcommonissuestheme: "",
+//        questionlevelfeedback: "",
+//        questiontype: "",
+//        questiontexteditorcomments: "",
+//        questionaudio: "",
+//        questionimagedescription: "",
+//        questionrubric: "",
+//        answertext1: "",
+//        answer1feedback: "",
+//        answer1audio: "",
+//        answer1imagedescription: "",
+//        answertext2: "",
+//        answer2feedback: "",
+//        answer2audio: "",
+//        answer2imagedescription: "",
+//        answertext3: "",
+//        answer3feedback: "",
+//        answer3audio: "",
+//        answer3imagedescription: "",
+//        answertext4: "",
+//        answer4feedback: "",
+//        answer4audio: "",
+//        answer4imagedescription: "",
+//        answertext5: "",
+//        answer5feedback: "",
+//        answer5audio: "",
+//        answer5imagedescription: "",
+//        answertext6: "",
+//        answer6feedback: "",
+//        answer6audio: "",
+//        answer6imagedescription: "",
+//        answereditorcomments: "",
+//        references: "",
+//        ERCentralLevel: "",
+//        ECCentralScore: "",
+//        Clausespersentence: "",
+//        wordcount: "",
+//        writername: "",
+//        peerreviewname: "",
+//        editorname: ""
+//
+//    };
+//}
 
 /**********************************************************
  *                   addTinyMCE()
@@ -334,67 +403,67 @@ function downloadit() {
 
 
 
-/******************************************************************
- *                validate()
- *
- * DESC: Checks the page for any issues we don't want to
- *       export. Returns a list of issues and marks them
- *       in the DOM to be fixed if the user desires.
- * INPUTS: None
- * RETURNS: {valid: bool, issues:[{issue:"",location:"",field:""}]}
- *****************************************************************/
-function validate() {
-    // Blank returned object.
-    var validity = {
-        valid: true,
-        issues: []
-    };
-    // Add an issue to the returned object.
-    function add_issue(l, f, i) {
-        validity.issues.push({
-            issue: i,
-            location: l,
-            field: f
-        })
-    }
-    // This example issue checks for the length of
-    //      passagetext and logs an issue if it is over 4000 chars.
-    for (var i = 0; i < file.length; i++) {
-        if (file[i].passagetext.length > 4000) {
-            console.log(file[i].passagetext.length)
-            add_issue(file[i].id, "passage text", "passagetext too long. It is " + file[i].passagetext.length + " characters long.")
-        }
-        if (file[i].passagefunction == "")
-            add_issue(file[i].id, "passage function", "passagefunction cannot be blank.");
-        if (file[i].topic == "")
-            add_issue(file[i].id, "topic", "topic cannot be blank.");
-        if (file[i].difficultylevel == "")
-            add_issue(file[i].id, "difficulty level", "difficultylevel cannot be blank.");
-        if (file[i].skill == "")
-            add_issue(file[i].id, "skill", "skill cannot be blank.");
-        if (file[i].level == "")
-            add_issue(file[i].id, "level", "level cannot be blank.");
-        if (file[i].questiontype == "")
-            add_issue(file[i].id, "question type", "questiontype cannot be blank.");
-        if (file[i].questionfunction == "")
-            add_issue(file[i].id, "question function", "questionfunction cannot be blank.");
-    }
-    // Mark any invalid inputs with the "invalid" class.
-    for (var i = 0; i < validity.issues.length; i++){
-        var issue = validity.issues[i];
-        var idTags = document.querySelectorAll(".uuid");
-        var location;
-        for (var j = 0; j < idTags.length; j++){
-            if (idTags[j].dataset.uuid == issue.location){
-                location = idTags[j];
-            }
-        }
-        location = location.closest('.row').querySelector('[data-label="' + issue.field + '"]');
-        location.classList.add('invalid');
-    }
-    // If there are issues, set "valid" to false.
-    if (validity.issues.length > 0) {
-        validity.valid = false;
-    }
-    return validity;
-}
+///******************************************************************
+// *                validate()
+// *
+// * DESC: Checks the page for any issues we don't want to
+// *       export. Returns a list of issues and marks them
+// *       in the DOM to be fixed if the user desires.
+// * INPUTS: None
+// * RETURNS: {valid: bool, issues:[{issue:"",location:"",field:""}]}
+// *****************************************************************/
+//function validate() {
+//    // Blank returned object.
+//    var validity = {
+//        valid: true,
+//        issues: []
+//    };
+//    // Add an issue to the returned object.
+//    function add_issue(l, f, i) {
+//        validity.issues.push({
+//            issue: i,
+//            location: l,
+//            field: f
+//        })
+//    }
+//    // This example issue checks for the length of
+//    //      passagetext and logs an issue if it is over 4000 chars.
+//    for (var i = 0; i < file.length; i++) {
+//        if (file[i].passagetext.length > 4000) {
+//            console.log(file[i].passagetext.length)
+//            add_issue(file[i].id, "passage text", "passagetext too long. It is " + file[i].passagetext.length + " characters long.")
+//        }
+//        if (file[i].function == "")
+//            add_issue(file[i].id, "function", "function cannot be blank.");
+//        if (file[i].topic == "")
+//            add_issue(file[i].id, "topic", "topic cannot be blank.");
+//        if (file[i].difficultylevel == "")
+//            add_issue(file[i].id, "difficulty level", "difficultylevel cannot be blank.");
+//        if (file[i].skill == "")
+//            add_issue(file[i].id, "skill", "skill cannot be blank.");
+//        if (file[i].level == "")
+//            add_issue(file[i].id, "level", "level cannot be blank.");
+//        if (file[i].questiontype == "")
+//            add_issue(file[i].id, "question type", "questiontype cannot be blank.");
+//        if (file[i].questionfunction == "")
+//            add_issue(file[i].id, "question function", "questionfunction cannot be blank.");
+//    }
+//    // Mark any invalid inputs with the "invalid" class.
+//    for (var i = 0; i < validity.issues.length; i++){
+//        var issue = validity.issues[i];
+//        var idTags = document.querySelectorAll(".uuid");
+//        var location;
+//        for (var j = 0; j < idTags.length; j++){
+//            if (idTags[j].dataset.uuid == issue.location){
+//                location = idTags[j];
+//            }
+//        }
+//        location = location.closest('.row').querySelector('[data-label="' + issue.field + '"]');
+//        location.classList.add('invalid');
+//    }
+//    // If there are issues, set "valid" to false.
+//    if (validity.issues.length > 0) {
+//        validity.valid = false;
+//    }
+//    return validity;
+//}
