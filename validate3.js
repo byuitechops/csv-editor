d6b8a338de6ee4f5602f66a50185cc6df95d978e
Didymus <didymus.benson@gmail.com>
2017-11-15 10:48:53 -0700

    Fixed a problem where deleted rows throw errors when downloading.

--- validate3.js
+++ validate3.js
@@ -24,32 +24,36 @@ function validate() {
     // This example issue checks for the length of
     //      passagetext and logs an issue if it is over 4000 chars.
     for (var i = 0; i < file.length; i++) {
-        if (file[i].passagetext.length > 4000) {
-            console.log(file[i].passagetext.length)
-            add_issue(file[i].id, "passage text", "passagetext too long. It is " + file[i].passagetext.length + " characters long.")
+        //If the row is being removed, don't worry about it.
+        if (!file[i].toDelete) {
+
+            if (file[i].passagetext.length > 4000) {
+                console.log(file[i].passagetext.length)
+                add_issue(file[i].id, "passage text", "passagetext too long. It is " + file[i].passagetext.length + " characters long.")
+            }
+            if (file[i].function == "")
+                add_issue(file[i].id, "function", "function cannot be blank.");
+            if (file[i].topic == "")
+                add_issue(file[i].id, "topic", "topic cannot be blank.");
+            if (file[i].difficultylevel == "")
+                add_issue(file[i].id, "difficulty level", "difficultylevel cannot be blank.");
+            if (file[i].skill == "")
+                add_issue(file[i].id, "skill", "skill cannot be blank.");
+            if (file[i].level == "")
+                add_issue(file[i].id, "level", "level cannot be blank.");
+            if (file[i].questiontype == "")
+                add_issue(file[i].id, "question type", "questiontype cannot be blank.");
+            if (file[i].questionfunction == "")
+                add_issue(file[i].id, "question function", "questionfunction cannot be blank.");
         }
-        if (file[i].function == "")
-            add_issue(file[i].id, "function", "function cannot be blank.");
-        if (file[i].topic == "")
-            add_issue(file[i].id, "topic", "topic cannot be blank.");
-        if (file[i].difficultylevel == "")
-            add_issue(file[i].id, "difficulty level", "difficultylevel cannot be blank.");
-        if (file[i].skill == "")
-            add_issue(file[i].id, "skill", "skill cannot be blank.");
-        if (file[i].level == "")
-            add_issue(file[i].id, "level", "level cannot be blank.");
-        if (file[i].questiontype == "")
-            add_issue(file[i].id, "question type", "questiontype cannot be blank.");
-        if (file[i].questionfunction == "")
-            add_issue(file[i].id, "question function", "questionfunction cannot be blank.");
     }
     // Mark any invalid inputs with the "invalid" class.
-    for (var i = 0; i < validity.issues.length; i++){
+    for (var i = 0; i < validity.issues.length; i++) {
         var issue = validity.issues[i];
         var idTags = document.querySelectorAll(".uuid");
         var location;
-        for (var j = 0; j < idTags.length; j++){
-            if (idTags[j].dataset.uuid == issue.location){
+        for (var j = 0; j < idTags.length; j++) {
+            if (idTags[j].dataset.uuid == issue.location) {
                 location = idTags[j];
             }
         }
